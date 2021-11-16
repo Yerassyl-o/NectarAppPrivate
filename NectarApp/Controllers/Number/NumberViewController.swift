f8dc988c9c55952184269d93bb92707b7761fdf7
Yerassyl <yerassyl.git@gmail.com>
2021-11-16 23:22:10 +0600

    number page willdid finish

---
+++ NectarApp/Controllers/Number/NumberViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NumberViewController.swift
+//  NectarApp
+//
+//  Created by Yerassyl Orazbekov on 16.11.2021.
+//
+
+import UIKit
+
+class NumberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
