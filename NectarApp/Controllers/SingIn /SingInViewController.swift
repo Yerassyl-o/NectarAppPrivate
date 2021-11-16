3da467d8235d741fa6a7d9e67905d7977f693da9
Yerassyl <yerassyl.git@gmail.com>
2021-11-16 18:35:51 +0600

    first page + sign in + number page created

---
+++ NectarApp/Controllers/SingIn /SingInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SingInViewController.swift
+//  NectarApp
+//
+//  Created by Yerassyl Orazbekov on 16.11.2021.
+//
+
+import UIKit
+
+class SingInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.barTintColor = UIColor.clear
+    }
+}
