e034e01f057d5a76b5c86d20a6fbdc1f0c5d5efe
Yerassyl <yerassyl.git@gmail.com>
2021-11-18 18:38:41 +0600

    i don't now write in this commit

--- NectarApp/Controllers/LogIn/LogInViewController.swift
+++ NectarApp/Controllers/LogIn/LogInViewController.swift
@@ -8,22 +8,91 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-
+    var loginSecureText = true
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var secureNoSecureButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var singUpButton: UIButton!
+    @IBOutlet weak var bottomPassword1pxView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        keyBoardSetUp()
+        setUpnumberEnterFieldPassword()
+    }
+    @IBAction func secureNoSecureAction(_ sender: Any) {
+        setUpsecureNoSecure()
+    }
+}
 
-        // Do any additional setup after loading the view.
+//MARK: SetUp keyboard
+extension LogInViewController {
+    func keyBoardSetUp() {
+        setupToHideKeyboardOnTapOnView()
+    }
+    func setupToHideKeyboardOnTapOnView() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(dismissKeyboard))
+
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+extension LogInViewController{
+    
+}
+
+extension LogInViewController: UITextFieldDelegate {
+    func setUpsecureNoSecure(){
+        if loginSecureText == true {
+            passwordTextField.isSecureTextEntry = false
+            loginSecureText = false
+        } else if loginSecureText == false {
+            passwordTextField.isSecureTextEntry = true
+            loginSecureText = true
+        }
+    }
+    func setUpnumberEnterFieldPassword(){
+        passwordTextField.keyboardType = .alphabet
+        passwordTextField.isSecureTextEntry = true
+        self.passwordTextField.delegate = self
+        passwordTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+    }
+        
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        passwordTextField.becomeFirstResponder()
+        self.view.endEditing(true)
+        return false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        numberEnterSetup()
     }
-    */
-
+    
+    func numberEnterSetup() {
+        if passwordTextField.text!.count <= 7 && passwordTextField.text!.count > 0 {
+            bottomPassword1pxView.backgroundColor = .systemRed
+            
+        } else if passwordTextField.text!.count == 0 {
+            bottomPassword1pxView.backgroundColor = UIColor(red: 226/255, green: 226/255, blue: 226/255, alpha: 1)
+        } else if passwordTextField.text!.count > 7 {
+            bottomPassword1pxView.backgroundColor = .systemGreen
+        }
+    }
 }
+
+
+
+
+
+    
+    
+
--- NectarApp/Controllers/SingIn /SingInViewController.swift
+++ NectarApp/Controllers/SingIn /SingInViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class SingInViewController: UIViewController {
-
+    var logic = [true, true, true, true]
+    
+    @IBOutlet weak var numberEnterField: UITextField!
+    @IBOutlet weak var bottom1pxView: UIView!
+    @IBOutlet weak var mainTitleLabel: UILabel!
+    @IBOutlet weak var locationButton: UIButton!
+    @IBOutlet weak var OrSoialMediaText: UILabel!
+    @IBOutlet weak var googleButton: UIButton!
+    @IBOutlet weak var facebookButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setUpnumberEnterField()
+        keyBoardSetUp()
+    }
+    @IBAction func locationButtonAction(_ sender: Any) {
+    }
+    @IBAction func contnueGoogleAction(_ sender: Any) {
+    }
+    @IBAction func contnueFacebookAction(_ sender: Any) {
     }
 }
+
+
+
+
+
 
 
 extension SingInViewController {
@@ -21,5 +42,83 @@ extension SingInViewController {
         let backItem = UIBarButtonItem()
         backItem.title = ""
         navigationItem.backBarButtonItem = backItem
+    }
+}
+
+extension SingInViewController: UITextFieldDelegate {
+    
+    func setUpnumberEnterField(){
+        numberEnterField.keyboardType = .numberPad
+        self.numberEnterField.delegate = self
+        numberEnterField.text = "+77"
+        numberEnterField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+
+    }
+        
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            self.view.endEditing(true)
+            return false
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        numberEnterSetup()
+    }
+    
+    func numberEnterSetup() {
+        if numberEnterField.text!.count <= 15 && numberEnterField.text!.count > 1 {
+            switch numberEnterField.text!.count {
+            case 2: numberEnterField.text! = "+77"
+            case 5: if logic[0] == true {
+                numberEnterField!.text = numberEnterField.text! + "-"
+                logic[0] = false
+                print(logic[0])
+            } else if logic[0] == false {
+                numberEnterField.deleteBackward()
+                logic[0] = true
+                print(logic[0])
+            }
+            bottom1pxView.backgroundColor = .systemRed
+            case 9: if logic[1] == true {
+                numberEnterField!.text = numberEnterField.text! + "-"
+                logic[1] = false
+            } else if logic[1] == false {
+                numberEnterField.deleteBackward()
+                logic[1] = true
+            }
+            case 12: if logic[2] == true {
+                numberEnterField!.text = numberEnterField.text! + "-"
+                logic[2] = false
+            } else if logic[2] == false {
+                numberEnterField.deleteBackward()
+                logic[2] = true
+            }
+            bottom1pxView.backgroundColor = .systemRed
+            case 15: bottom1pxView.backgroundColor = .systemGreen
+            default: break
+            }
+        } else if numberEnterField.text!.count > 15 && numberEnterField.text!.count > 1 {
+            numberEnterField.deleteBackward()
+        }
+        else if numberEnterField.text!.count == 3 {
+            bottom1pxView.backgroundColor = UIColor(red: 226/255, green: 226/255, blue: 226/255, alpha: 1)
+        }
+    }
+}
+
+extension SingInViewController {
+    func keyBoardSetUp() {
+        setupToHideKeyboardOnTapOnView()
+    }
+    func setupToHideKeyboardOnTapOnView() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(dismissKeyboard))
+
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
 }
